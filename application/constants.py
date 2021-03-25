1bd51c2f18425598529b260b7891462e8dc3e118
audran <audranthomas69@gmail.com>
2021-03-25 21:02:52 +0100

    rollback conenction strin

--- application/constants.py
+++ application/constants.py
@@ -24,21 +24,8 @@ ADDRESS_ALGO_OURSELF = 'HKDGSHRLJJLQP463PPTGIRQWMSPOIWR5CGAOCKOHOEH3WEU44SEYIDHP
 PROFILE_PICTURES_CONTAINER = "profile"
 
 
-# CONNECTION_STRING = "Driver={ODBC Driver 17 for SQL Server};Server=tcp:starpicserver.database.windows.net,1433;Database=starpic;Uid=main_admin;Pwd=Password1;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;"
+CONNECTION_STRING = "Driver={ODBC Driver 17 for SQL Server};Server=tcp:starpicserver.database.windows.net,1433;Database=starpic;Uid=main_admin;Pwd=Password1;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;"
 APPLICATION_INSIGHT_MAIL_URL = "https://prod-05.francecentral.logic.azure.com:443/workflows/99dbadb3eca946519f25e7829" \
                                "d1e0b77/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2" \
                                "Frun&sv=1.0&sig=QUnZEinH4u1wra3bJ9nPakFxOkt5DYTYaMbQ_A-jpcI"
 BLOB_CONNECTION_STRING = "DefaultEndpointsProtocol=https;AccountName=mypic;AccountKey=Po7bUVzGfC8V00KLcA02loMAOg60TN+/nB8hGQf9rQvOc2sQmKKahZ7xvBmIiAFgSqxDIGv1YUGlTha62bAejQ==;EndpointSuffix=core.windows.net"
-
-
-from azure.keyvault.secrets import SecretClient
-from azure.identity import ManagedIdentityCredential
-
-keyVaultName = 'mypic-keyvault-prd'
-KVUri = f"https://{keyVaultName}.vault.azure.net"
-
-# credential = DefaultAzureCredential()
-credential = ManagedIdentityCredential()
-client = SecretClient(vault_url=KVUri, credential=credential)
-secret = client.get_secret("database-conn")
-CONNECTION_STRING = secret.value
